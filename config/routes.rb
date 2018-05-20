a5501ed88ec6be7b5d8aa832f1c5f166918bd681
Carol Greene <carolgreene3@gmail.com>
2018-05-20 22:30:20 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Rails.application.routes.draw do
 
   Rails.application.routes.draw do
     resources :coupons, only: [:index, :new, :create]
-    get '/coupon/:id', to: 'coupons#show', as 'coupon'
+    get '/coupon/:id', to: 'coupons#show', as: 'coupon'
   end
 
 
